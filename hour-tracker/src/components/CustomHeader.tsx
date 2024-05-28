9883db99120882f3979a39948a3969424118932b
Owen Mariani <mariani.owen@gmail.com>
2024-05-28 15:57:24 -0400

    user info in sider complete

--- hour-tracker/src/components/CustomHeader.tsx
+++ hour-tracker/src/components/CustomHeader.tsx
@@ -4,22 +4,45 @@ import Image from 'next/image';
 import icon from '../../public/full_logo.png'
 import { PageProps } from '@/common/interfaces';
 import { buRed, accentBackground } from '@/common/styles';
+import Pfp from "./Pfp";
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const CustomHeader: React.FC<PageProps> = ({ session }) => {
 
 
-  let pfp = "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg";
-    if(session?.user?.image){
-         pfp = session.user.image
-    }
+  
 
   return (
-    <Header style={{ height: '4em', zIndex: 4, background: accentBackground, borderBottom: `0.3rem solid ${buRed}`, paddingLeft: '0.2rem', display: 'flex', alignItems: 'center', margin: 0 }}>
-    <Image src={icon} alt="logo" style={{ height: '3rem', width: 'auto', paddingTop: '0.5rem' }} />
-    <img src={pfp} alt="pfp" style={{ marginRight: '-1rem',marginLeft: 'auto', height: '2.5rem', width: 'auto', borderRadius: '50%', border: `0.1rem solid ${buRed}` }} />
-  </Header>
+    <Header
+      style={{
+        height: "4em",
+        zIndex: 4,
+        background: accentBackground,
+        borderBottom: `0.3rem solid ${buRed}`,
+        paddingLeft: "0.2rem",
+        display: "flex",
+        alignItems: "center",
+        margin: 0,
+      }}
+    >
+      <Image
+        src={icon}
+        alt="logo"
+        style={{ height: "3rem", width: "auto", paddingTop: "0.5rem" }}
+      />
+      <div
+        style={{
+          marginRight: "-1rem",
+          marginLeft: "auto",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Pfp session={session} height={"2.5rem"} />
+      </div>
+    </Header>
   );
 };
 
---
+++ hour-tracker/src/components/Pfp.tsx
@@ -0,0 +1,25 @@
+import e from "express";
+import { PageProps } from "../common/interfaces";
+import { buRed } from "../common/styles";
+
+const Pfp: React.FC<any> = ({ session, height: dimension }) => {
+  let pfp =
+    "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg";
+  if (session?.user?.image) {
+    pfp = session.user.image;
+  }
+  return (
+    <img
+      src={pfp}
+      alt="pfp"
+      style={{
+        height: dimension,
+        width: dimension,
+        borderRadius: "50%",
+        border: `0.1rem solid ${buRed}`,
+      }}
+    />
+  );
+};
+
+export default Pfp;
