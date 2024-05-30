f7d759e3b3fcad34b3a28bc8dad73d5fa8619ef3
Owen Mariani <mariani.owen@gmail.com>
2024-05-30 12:45:15 -0400

    added useEffect to pfp so that it is rendered at the correct time

--- hour-tracker/src/components/Pfp.tsx
+++ hour-tracker/src/components/Pfp.tsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import { buRed } from "../common/styles";
 
 const Pfp: React.FC<any> = ({ session, height: dimension }) => {
-  let pfp =
-    "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg";
-  if (session?.user?.image) {
-    pfp = session.user.image;
-  }
+  const [pfp, setPfp] = useState(
+    "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg"
+  );
+
+  useEffect(() => {
+    const getPfp = async () => {
+      if (session?.user?.image) {
+        return session.user.image;
+      }
+      return "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg";
+    };
+
+    const fetchPfp = async () => {
+      const imageUrl = await getPfp();
+      setPfp(imageUrl);
+    };
+
+    fetchPfp();
+  }, [session]);
+
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
