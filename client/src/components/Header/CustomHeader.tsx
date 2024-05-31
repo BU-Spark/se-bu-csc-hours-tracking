1eb4a5872e8d89f39d441fee11439ae0a663009b
Owen Mariani <mariani.owen@gmail.com>
2024-05-31 15:02:36 -0400

    removed session from header

--- client/src/components/Header/CustomHeader.tsx
+++ client/src/components/Header/CustomHeader.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Layout } from "antd";
 import Image from "next/image";
 import icon from '../../../../public/full_logo.png'
-import { PageProps } from "../../common/interfaces";
 import "./CustomHeader.css";
 import { accentBackground, buRed } from "../../common/styles";
 import Pfp from "../Pfp";
 
 const { Header } = Layout;
 
-const CustomHeader: React.FC<PageProps> = () => {
+const CustomHeader: React.FC = () => {
   return (
     <Header
     className="fixedHeader"
