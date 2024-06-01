4722966b10b20caac13485e767b8bc59f0de0915
Owen Mariani <mariani.owen@gmail.com>
2024-06-01 14:38:20 -0400

    made header component to use for each page

--- client/src/components/BodyHeader.tsx
+++ client/src/components/BodyHeader.tsx
@@ -3,28 +3,24 @@ import { Typography } from "antd"
 import { usePathname } from "next/navigation"
 
 const BodyHeader: React.FC = () => {
-    const formatPath = () =>{
-        const path = usePathname()
-        if(path != null || undefined){
-            if(path?.length != null && path.length > 1){
-                const formatted = path[1].toUpperCase() + path.substring(2)
-                return formatted
-            }
-            else{
-                return 'Dashboard'
-            }
+    const useFormatPath = () => {
+      const path = usePathname();
+      if (path != null || undefined) {
+        if (path?.length != null && path.length > 1) {
+          const formatted = path[1].toUpperCase() + path.substring(2);
+          return formatted;
+        } else {
+          return "Dashboard";
         }
-        
-    }
+      }
+    };
 
-    return(
-        //known issue: the text is a huge h1 for some reason
-       <>
-       <h3>{formatPath()}</h3>
-       </> 
-         
-       
-    )
+    return (
+      //known issue: the text is a huge h1 for some reason
+      <>
+        <h3>{useFormatPath()}</h3>
+      </>
+    );
 }
 
 export default BodyHeader
