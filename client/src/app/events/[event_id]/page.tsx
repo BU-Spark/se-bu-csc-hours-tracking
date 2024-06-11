e3a8df3253cca77af6fcbd30862f4a71bc072d00
Owen Mariani <mariani.owen@gmail.com>
2024-06-11 17:59:17 -0400

    replace title with category for header for eventcards

--- client/src/app/events/[event_id]/page.tsx
+++ client/src/app/events/[event_id]/page.tsx
@@ -56,34 +56,6 @@ export default function Page() {
     fetchEvent();
   }, [event_id]);
 
-  const formatDate = (date: Date, hasTime: boolean) => {
-    const timeOptions: Intl.DateTimeFormatOptions = {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    };
-
-    const nonTimeOptions: Intl.DateTimeFormatOptions = {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    };
-    const option = hasTime ? timeOptions : nonTimeOptions;
-    return date.toLocaleDateString("en-US", option);
-  };
-
-  const formatTime = (date: Date) => {
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const period = hours >= 12 ? "PM" : "AM";
-    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-
-    return `${formattedHours}:${formattedMinutes} ${period}`;
-  };
-
   return event ? (
     <div
       style={{
