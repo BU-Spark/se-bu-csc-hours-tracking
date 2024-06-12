5051cdc2534a78f85a1a7348f54521b513f89334
Owen Mariani <mariani.owen@gmail.com>
2024-06-12 16:56:27 -0400

    moved formatdate and time to new file for prod

--- client/src/app/events/[event_id]/page.tsx
+++ client/src/app/events/[event_id]/page.tsx
@@ -9,34 +9,10 @@ import e from "express";
 import { CalendarOutlined, ClockCircleOutlined } from "@ant-design/icons";
 import { buRed } from "@/common/styles";
 import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
+import { formatDate, formatTime } from "@/app/utilities/DateFormatters";
 
-export const formatDate = (date: Date, hasTime: boolean) => {
-  const timeOptions: Intl.DateTimeFormatOptions = {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  };
 
-  const nonTimeOptions: Intl.DateTimeFormatOptions = {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  };
-  const option = hasTime ? timeOptions : nonTimeOptions;
-  return date.toLocaleDateString("en-US", option);
-};
 
-export const formatTime = (date: Date) => {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const period = hours >= 12 ? "PM" : "AM";
-  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-
-  return `${formattedHours}:${formattedMinutes} ${period}`;
-};
 
 export default function Page() {
   const [event, setEvent] = useState<Event>();
