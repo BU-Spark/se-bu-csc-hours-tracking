2a43b7612fb5ef868ce866e77745df7584e6e5bc
Owen Mariani <mariani.owen@gmail.com>
2024-06-17 11:17:23 -1000

    fixed hoursubmission mapping

--- client/src/app/my-hours/action.ts
+++ client/src/app/my-hours/action.ts
@@ -1,24 +1,7 @@
 "use server";
 
-import prisma from '../../lib/prisma';
-
-interface HourSubmission {
-  id: number;
-  hours: number;
-  date_submitted: Date;
-  approval_status: number;
-  event: Event;
-  updated_by?: { name: string };
-  description: string;
-  note: string;
-}
-
-interface Event {
-  id: number;
-  title: string;
-  location: string;
-  image: string;
-}
+import { Event, HourSubmission } from "@prisma/client";
+import prisma from "../../lib/prisma";
 
 export const getHoursByUserEmail = async (email: string) => {
   const user = await prisma.person.findUnique({
@@ -37,16 +20,34 @@ export const getHoursByUserEmail = async (email: string) => {
     return [];
   }
 
-  return user.hour_submissions.map((submission: HourSubmission) => ({
-    id: submission.id,
-    image: submission.event.image || '/default.png',
-    eventName: submission.event.title,
-    location: submission.event.location,
-    status: submission.approval_status === 1 ? 'approved' : 'pending',
-    date: submission.date_submitted.toString(),
-    reviewer: submission.updated_by && submission.updated_by.name !== user.name ? submission.updated_by.name : 'N/A',
-    hours: submission.hours,
-    description: submission.description,
-    feedback: submission.note,
-  }));
+  const hourSubmissionsWithEventDetails = await Promise.all(
+    user.hour_submissions.map(async (submission: HourSubmission) => {
+      const event: Event | null = await prisma.event.findUnique({
+        where: { id: submission.event_id },
+      });
+
+      if (!event) {
+        return null;
+      }
+
+      return {
+        id: submission.id,
+        image: event.image.toString("base64"),
+        eventName: event.title,
+        location: event.location,
+        status: submission.approval_status === 1 ? "approved" : "pending",
+        date: submission.date_submitted.toString(),
+        reviewer:
+          submission.updated_by_id !== user.id
+            ? submission.updated_by_id
+            : "N/A",
+        hours: submission.hours,
+        description: submission.description,
+        feedback: submission.note,
+      };
+    })
+  );
+
+  // Filter out any null values in case an event was not found
+  return hourSubmissionsWithEventDetails.filter(Boolean);
 };
