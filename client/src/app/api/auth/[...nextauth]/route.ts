7843e124086a486a08a2b95d9f10b2fa8f710e86
Owen Mariani <mariani.owen@gmail.com>
2024-06-21 15:19:36 -1000

    tweaking route.ts export

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -99,5 +99,4 @@ export const authOptions: NextAuthOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
