c5586bc5bfc4bb8c4a0a02ada5eba2880f5fd04d
Owen Mariani <mariani.owen@gmail.com>
2024-06-21 15:42:19 -1000

    fixed auth option import

--- client/src/app/(user)/user/settings/action.ts
+++ client/src/app/(user)/user/settings/action.ts
@@ -2,7 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export const checkIfNewUser = async () => {
   const session = await getServerSession(authOptions);
