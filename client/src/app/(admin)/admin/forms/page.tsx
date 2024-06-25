b4bdf55ad9fd05ea829c3f9288d639b95e3b86a2
Owen Mariani <mariani.owen@gmail.com>
2024-06-25 09:45:57 -0400

    made add form button ui

--- client/src/app/(admin)/admin/forms/page.tsx
+++ client/src/app/(admin)/admin/forms/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 import React from "react";
+import StyledButton from "@/components/StyledButton";
 
 const Forms: React.FC = () => {
-  return <div>Forms</div>;
+  return (
+    <div>
+      <StyledButton
+        text="+ Add Form"
+        selected={true}
+        onClick={() => {
+          console.log("log");
+        }}
+      />
+    </div>
+  );
 };
 
 export default Forms;
