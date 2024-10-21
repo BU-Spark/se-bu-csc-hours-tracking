4e089beff405c5326cc9514396fbc7109dec5510
whalewatcher98 <jzhou04020@gmail.com>
2024-10-21 17:10:46 -0400

    copied over from admin pending-hours

--- client/src/app/(third-party)/third-party/submissions/page.tsx
+++ client/src/app/(third-party)/third-party/submissions/page.tsx
@@ -1,10 +1,120 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import CustomTable from "@/components/Table/CustomTable";
+import {
+  HeaderOffset,
+  SummaryContainer,
+  SummaryBox,
+} from "@/_common/styledDivs";
+import { useState, useEffect } from "react";
+import {
+  EventApplicationTableParams,
+  EventApplicationsTableData,
+  HoursTableData,
+} from "@/interfaces/interfaces";
+import { CustomTableParams } from "@/interfaces/interfaces";
+import StyledButton from "@/components/StyledButton";
+import {
+  getAllPendingApplications,
+  getEventApplicationsTableData,
+} from "./action";
+import { Application } from "@prisma/client";
+import EventApplicationTable from "./EventApplicationTable";
 
 const Submissions: React.FC = () => {
-    return(
-        <div>third party submissions</div>
-    )
-}
+  const [showHistory, setShowHistory] = useState<boolean>(false);
+  const [pendingApplications, setPendingApplications] = useState<
+    EventApplicationsTableData[]
+  >([]);
+  const [reviewedApplications, setReviewedApplications] = useState<
+    EventApplicationsTableData[]
+  >([]);
+
+  const input: EventApplicationTableParams = {
+    data: showHistory ? reviewedApplications : pendingApplications,
+    set1: setPendingApplications,
+    val1: pendingApplications,
+    set2: setReviewedApplications,
+    val2: reviewedApplications,
+  };
+
+  useEffect(() => {
+    const fetchAllApplications = async () => {
+      const response:
+        | {
+            pendingApplicationRows: EventApplicationsTableData[];
+            reviewedApplicationRows: EventApplicationsTableData[];
+          }
+        | undefined = await getEventApplicationsTableData();
+      if (!response) {
+        console.error("bad response");
+        return;
+      }
+
+      setPendingApplications(response.pendingApplicationRows);
+      setReviewedApplications(response.reviewedApplicationRows);
+      // console.log(response.pendingApplicationRows);
+    };
+
+    fetchAllApplications();
+  }, []);
+  return (
+    <HeaderOffset>
+      <SummaryContainer
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "inherit",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "end",
+            width: "30%",
+            margin: "1rem 0",
+          }}
+        >
+          <StyledButton
+            text="Pending"
+            onClick={() => {
+              setShowHistory(false);
+            }}
+            selected={showHistory == false}
+          />
+          <StyledButton
+            text="History"
+            onClick={() => {
+              setShowHistory(true);
+            }}
+            selected={showHistory == true}
+          />
+        </div>
+        <div
+          style={{
+            gap: "2rem",
+            display: "flex",
+            justifyContent: "flex-end",
+            width: "100%",
+          }}
+        >
+          <SummaryBox>
+            <h2>
+              {pendingApplications ? pendingApplications.length.toString() : 0}
+            </h2>
+            <p>Pending Applications</p>
+          </SummaryBox>
+        </div>
+      </SummaryContainer>
+      <EventApplicationTable
+        data={input.data}
+        set1={input.set1}
+        val1={input.val1}
+        set2={input.set2}
+        val2={input.val2}
+      />
+    </HeaderOffset>
+  );
+};
 
 export default Submissions;
