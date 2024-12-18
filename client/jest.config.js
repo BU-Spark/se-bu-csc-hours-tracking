f6558b7e084654eb777765330e0a1be76acc59ef
Alex Miller <amiller06880@gmail.com>
2024-12-17 20:41:13 -0500

    cleaned up comments in config file

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,13 +1,11 @@
-// jest.config.js
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  dir: './', // path to your Next.js app
+  dir: './',
 });
 
 const customJestConfig = {
   testEnvironment: 'jsdom',
-  // Add any custom configuration as needed
 };
 
 module.exports = createJestConfig(customJestConfig);
