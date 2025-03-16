1222626e04784936c79f7712078fb35f2aa2a902
alvin <alvinzhu65@gmail.com>
2025-03-15 22:35:05 -0400

    made cards more dynamic, can pass props in now.

--- client/src/app/(admin)/admin/dashboard/page.tsx
+++ client/src/app/(admin)/admin/dashboard/page.tsx
@@ -1,8 +1,14 @@
 "use client";
-import { Card } from 'antd';
+import PendingCard from '../dashboard/PendingCard'
 
 export default function Dashboard(){
+    const people = [
+        {name : "Sam Crissman", year : "CAS 26'", category: "SFR", hours : 4, date: "03/25/2024"}, 
+        {name : "Alex Smith", year : "CAS 25'", category : "SFR", hours : 5, date : "03/26/2924"}, 
+        {name : "Jamie Doe", year : "CAS 27'",category: "SFR", hours : 3, date : "03/24/2024"} 
+    ]
     return(
+
         <>
         <h1 style={{ fontSize: "clamp(1.2rem, 2vw, 2rem)" }}>Dashboard</h1>
         <div>
@@ -10,72 +16,13 @@ export default function Dashboard(){
             Pending Hour Approvals
             </strong>
         </div>
-
-        <Card 
-            style={{ 
-                marginTop: "1.25rem",  
-                width: "clamp(12rem, 30vw, 18rem)",
-                backgroundColor: '#CC0000', 
-                color: 'white', 
-                borderRadius: "1.5625rem", 
-                border: "0.125rem solid #CC0000"  
-            }} 
-            title={
-            <div style={{ display: 'flex', flexDirection: 'row', marginTop: "0.1875rem" }}> 
-                <img 
-                    src="https://i.pravatar.cc/40" 
-                    style={{ 
-                        height: "clamp(2.5rem, 5vw, 3rem)",  
-                        width: "clamp(2.5rem, 5vw, 3rem)",
-                        borderRadius: "50%" 
-                    }}
-                />
-                <div style={{ display: 'flex', flexDirection: 'column', marginLeft: "0.625rem" }}>  
-                    <span style={{ color:'white', fontSize: "clamp(0.8rem, 1.5vw, 1rem)" }}>Sam Crissman</span>
-                    <span style={{ fontSize: "clamp(0.656rem, 1.3vw, 0.75rem)", color:'white' }}>CAS 26'</span> 
-                </div>
-            </div>}
-            styles={{
-                body: {
-                    backgroundColor: 'white',
-                    color: 'black', 
-                    borderBottomLeftRadius: "1.5625rem",  
-                    borderBottomRightRadius: "1.5625rem",
-                }, 
-                header: {
-                    borderTopLeftRadius: "1.5625rem",
-                    borderTopRightRadius: "1.5625rem",
-                    height: "4.6875rem", 
-                    marginLeft: "-0.9375rem"  
-                }, 
-            }}
-        >
-            <div style={{ marginTop: "-1.875rem", marginBottom: "-0.9375rem" }}> 
-                <ul style={{ 
-                    marginLeft: "-3.125rem",  
-                    listStyle: 'none', 
-                    display: 'flex', 
-                    flexDirection: 'column', 
-                    gap: "0.3125rem" 
-                }}>
-                    <li><strong>Category: </strong>SFR</li>
-                    <li><strong>Hours: </strong>4</li>
-                    <li><strong>Date: </strong>03/25/2024</li>
-                    <a 
-                        href="#" 
-                        style={{ 
-                            marginTop: "0.3125rem",
-                            textDecoration: 'underline', 
-                            color: "#CC0000", 
-                            fontWeight: "bold",
-                            fontSize: "clamp(0.8rem, 1.5vw, 1rem)"
-                        }}
-                    >
-                        Full Profile
-                    </a>
-                </ul>
-            </div>
-        </Card>
+        <div style ={{display:'flex', flexDirection : 'row', gap : '15px'}}>
+            {people.map((person, index) => (
+                <PendingCard key = {index} year = {person.year} name = {person.name} category = {person.category} hours = {person.hours} date = {person.date}
+                profilePic = {"https://i.pravatar.cc/40?img=" + (index + 1)}/>
+            ))
+            }
+        </div>
         </>
     );
 }
